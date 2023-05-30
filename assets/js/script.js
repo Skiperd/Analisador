f4090421f36bf1c07673bc7c3acbb3872bed45e1
skiperd <skiperd2015@hotmail.com>
2023-05-30 17:01:30 -0300

    1.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,34 +2,40 @@ const btn = document.querySelector('.btn')
 const input = document.querySelector('.input')
 const resu = document.querySelector('.resu')
 
-
-let cadeia
+let cadeia = input.value
+let msg = "";
 
 const nvalido = ['j',"w","k","y","ç","h",'q','/','(',')','&','$','%','#','@','!']
 const vogais = ["a","e","i","o","u"]
 const reservados = /(z|x)/i;
-const consoantes = ["b","c","d","f","l","m","n","p","r","s","t","v","w","x","z"];
+const consoantes = ["b","c","d","f","g","l","m","n","p","r","s","t","v","w","x","z"];
 
 function checarValidos(cadeia) {
   cadeia = input.value;
   let valid = true;
 
-  if(cadeia.length > 10) {
-    console.log(`A Cadeia: ${cadeia} ultrapassou o limite de 10 caracteres`)
+  //VERIFICA O TAMANHO DA CADEIA
+  // if(cadeia.length > 10) {
+  //   // cadeia = cadeia.slice(0,9)
+  //   // msg = `A Cadeia: ${cadeianova} foi aceita e, o resto das letras ignorado`
+  //   valid = true;
+  //   console.log(cadeia)
+  //   return valid;
+  // }
+  //VERIFICA SE CONTÉM LETRAS PROIBIDAS
+   if(nvalido.some((letra) => cadeia.includes(letra))) {
+    msg = 'caracteres não permitidos na cadeia'
     valid = false;
     return valid;
   }
-  else if(nvalido.some((letra) => cadeia.includes(letra))) {
-    console.log('caracteres não permitidos na cadeia')
-    valid = false;
-    return valid;
-  }
+  //VERIFICA SE CONTEM LETRAS RESERVADAS Z e X
   else if(cadeia.match(reservados)) {
-    console.log(`Palavra ${cadeia} contém letras reservadas (Z|X)`)
+    msg = `Palavra ${cadeia} contém letras reservadas (Z|X)`
     valid = false;
     return valid;
   }
-
+  //ELE CHAMA DUAS FUNÇÕES, UMA CHECA SE CONTÉM NÚMEROS E A OUTRA CHECA A SEQUENCIA
+  //CONSOANTE VOGAL
   else if(checaSeContemNumero(cadeia) && sequenciaConsoanteVogal(cadeia)) {
     valid = true;
     return valid
@@ -37,56 +43,41 @@ function checarValidos(cadeia) {
    else {
     valid = false;
   }
-  // if(checaSeContemNumero(cadeia));
-  // if(sequenciaConsoanteVogal(cadeia));
-  // if(checarPrimeiroDigitio(cadeia));
-  // if(cadeia.startsWith('z') || cadeia.startsWith('x')) {
-  //   console.log('Palavras não podem ser iniciadas com Z ou X')
-  //   erro = true;  
-  // }
-//  return erro;
-
     return valid;
 }
 
-
+//FUNÇÃO PARA VERIFICAR A SEQUENCIA CONSOANTE VOGAL
 function sequenciaConsoanteVogal(frase) {
   let consoanteposi = false;
+  let n = 0;
   for(let i =0; i < frase.length; i++) {
     const letra = frase[i].toLowerCase();
     if(consoanteposi && vogais.includes(letra)) {
       consoanteposi = false;
     } else if (!consoanteposi && consoantes.includes(letra)) {
       consoanteposi = true;
-    } else {
+    } else if(i === frase.length -1 && !isNaN(parseInt(letra)) && n === 0) {
+      n++
+    } 
+    else {
+      msg = 'Sequencia não segue ordem consoante/vogal'
       return false;
     }
   }
   return true;
 }
 
-// function checarPrimeiroDigitio(cadeia) {
-//   let valid = true;
-//   let digito = +cadeia[0]
-//   if(!isNaN(digito)) {
-//     console.log('Cadeia não aceita. não pode iniciar com numero')
-//     valid = false;
-//     return valid;
-//   } else {
-//     return valid;
-//   }
-//   return erro;
-// }
-
+  //FUNÇÃO PARA TESTA SE CONTEM NÚMERO
   function contemNumero(n){
     return /\d/.test(n);
   }
+  //VERIFICA SE NA CADEIA CONTÉM NÚMERO, EXCLUINDO EVENTUAIS NÚMEROS NO ULTIMO ÍNDICE
   function checaSeContemNumero(cadeia){
     // let valid = true;
     let valid = true;
     let cadeiadiminuida = cadeia.slice(0,-1)
       if(contemNumero(cadeiadiminuida) === true) {
-        console.log('Cadeia não aceita, não pode conter números no inicio e meio')
+        msg = 'Cadeia não aceita, não pode conter números no inicio e meio';
         valid = false;
       } else {
         valid = true;
@@ -94,18 +85,22 @@ function sequenciaConsoanteVogal(frase) {
       return valid;
   }
 
+  
   function eValido() {
     // let cadeia = input.value;
     if(checarValidos()) {
-      resu.style.color = 'green'
-      resu.innerHTML = `A Sequencia "${input.value}" foi aceita`
-      return `Sequencia aceita`
+        resu.style.color = 'green'
+        resu.innerHTML = `A Sequencia "${input.value.slice(0,10)}" foi aceita`
+        console.log(cadeia)
+        return `Sequencia aceita`
+      
     } else {
       resu.style.color = 'red'
-      resu.innerHTML = `A Sequencia "${input.value}" não foi aceita`
+      resu.innerHTML = msg
       return `Sequencia não aceita`
     }
   }
+
 
 btn.addEventListener('click', function(e){
   e.preventDefault();
